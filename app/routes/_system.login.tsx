962253a56e7fa05dd16a21819edaaf1b0975048e
Rick Snackers <rsnackers@gmail.com>
2024-12-16 22:43:24 +0100

    Removed comment

--- app/routes/_system.login.tsx
+++ app/routes/_system.login.tsx
@@ -104,9 +104,6 @@ export default function LoginPage() {
             Je wordt ingelogd...
           </Button>
         )}
-        {/* <button className="mt-8 w-full rounded-md bg-sky-600 px-3 py-2 font-medium text-white hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-sky-600 focus:ring-offset-2 focus:ring-offset-gray-900">
-          Log in
-        </button> */}
       </Form>
     </>
   );
